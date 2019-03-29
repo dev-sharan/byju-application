1b9f34333605d0773449e91d89d2b156bf86e20d
dev-sharan <sharan.manish@ymail.com>
2019-03-30 03:01:51 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BjyuMaterialModule } from './bjyu-material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {HttpClientModule} from '@angular/common/http';
+import { DataService } from './data.service';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, BjyuMaterialModule, FlexLayoutModule, HttpClientModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [ DataService ]
 })
 export class AppModule { }
